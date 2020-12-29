95a9e0ad1b309f88321c9c6e7d57021fa59028df
ryanhlewis <>
2020-12-29 12:15:36 -0600

    Main commit

---
+++ AnotherOne/Assets/new/scripts/ScriptObjectMonsterSpawner.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+ using System.Collections;
+ using System.Collections.Generic;
+  
+ public class ScriptObjectMonsterSpawner : MonoBehaviour
+ {
+  
+     public GameObject monster;
+     public GameObject[] spawnPoints;
+     //public GameObject particle;
+     public List<GameObject> Children;
+     public int spawnAmount = 5;
+     public float spawnRate = 5.0f;
+     private bool spawning;
+  
+     // Use this for initialization
+     void Start()
+     {
+         Children = new List<GameObject>();
+         //^ Could initialise the size of this with the max number of enemies there would be too (if this is fixed)
+ 
+         spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+         //^ Moved this from the coroutine as you only need to find the spawn points once (and it's expensive)
+ 
+        // particle.SetActive(false);
+     }
+  
+     // Update is called once per frame
+     void Update()
+     {
+  
+         if (Children.Count < spawnAmount && spawning == false)
+         {
+             StartCoroutine(SpawnMonster());
+             Debug.Log("Spawning");        
+             
+              }
+     }
+  
+     public IEnumerator SpawnMonster()
+     {
+         spawning = true;
+         //particle.SetActive(true);
+         var index = Random.Range(0, spawnPoints.Length);
+         var currentPoint = spawnPoints[index];
+         var newmonster = Instantiate(monster, currentPoint.transform.position, currentPoint.transform.rotation) as GameObject;
+         //^Note that we could simplify the above since we don't use index or currentPoint for anything else, like so:
+         //var newmonster = Instantiate(monster, spawnPoints[Random.Range(0, spawnPoints.Length].transform.position
+         Children.Add(newmonster);
+         newmonster.transform.parent = gameObject.transform;
+         yield return new WaitForSeconds(spawnRate);
+         //particle.SetActive(false);
+         spawning = false;
+     }
+ }
---
+++ AnotherOne/Assets/new/scripts/sacrificeCheck.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sacrificeCheck : MonoBehaviour
+{
+public GameObject monster;
+public GameObject spawnPoint;
+int numberOfEnemiesTotal;
+    public GameObject gameManager;
+
+
+//shake
+
+    [SerializeField]
+    ShakeableTransform target;
+
+    [SerializeField]
+    float delay = 1f;
+
+    /// <summary>
+    /// Maximum possible stress that will be inflicted
+    /// on the target when it is immediately beside the
+    /// explosion.
+    /// </summary>
+    [SerializeField]
+    float maximumStress = 0.6f;
+
+    [SerializeField]
+    float range = 45;
+
+    bool runOnce = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //public int mushroomCount = 0;
+    }
+
+private void OnTriggerEnter(Collider other) {
+numberOfEnemiesTotal = gameManager.GetComponent<EnemyManager>().numberOfEnemiesRemaining;
+
+if(numberOfEnemiesTotal == 0)
+Instantiate(monster, spawnPoint.transform.position, spawnPoint.transform.rotation);
+
+}
+
+ private void OnTriggerStay(Collider other)
+    {
+        
+        if(gameManager.GetComponent<EnemyManager>().numberOfEnemiesRemaining < numberOfEnemiesTotal ){
+
+            if(!runOnce) {
+               Debug.Log("Enemy must be dead");    //Do sacrifice effect- lower wall?
+            runOnce = true;
+        //yield return new WaitForSeconds(delay);
+
+       // GetComponent<ParticleSystem>().Play();
+
+        float distance = Vector3.Distance(transform.position, target.transform.position);
+
+        // Retrieve a value in the 0...1 range, where it is 0 when
+        // the target is immediately beside us, and 1 when at (or beyond) maximum range.
+        float distance01 = Mathf.Clamp01(distance / range);
+
+        // Square the value to create a parabolic falloff curve,
+        // then invert it such that closer distances cause greater stress.
+        // https://en.wikipedia.org/wiki/Parabola
+        float stress = (1 - Mathf.Pow(distance01, 2)) * maximumStress;
+
+        target.InduceStress(stress);
+            }
+
+        }
+       // return false;
+
+    }
+
+
+}
